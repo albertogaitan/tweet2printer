1ce94478b50137e6959f9296ecc8fc75f131caae
Alberto Gaitán <alberto.gaitan@gmail.com>
2013-11-10 01:23:01 -0500

    Add two more working examples.

---
+++ tweets-to-mongodb.py
@@ -0,0 +1,33 @@
+import json
+import pymongo
+import tweepy
+
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+
+class CustomStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        super(tweepy.StreamListener, self).__init__()
+
+        self.db = pymongo.MongoClient().test
+
+    def on_data(self, tweet):
+        self.db.tweets.insert(json.loads(tweet))
+
+    def on_error(self, status_code):
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        return True # Don't kill the stream
+
+
+sapi = tweepy.streaming.Stream(auth, CustomStreamListener(api))
+sapi.filter(track=['Gandolfini'])
